203556c7327651fd4f228b2a99ce59936673ec77
Saumay Raj <saumayraj621@gmail.com>
2026-09-10 21:28:37 +0530

    Implement encapsulation in employee class

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+class employee
+{
+private:
+    int id;
+    string name;
+    float salary;
+
+public:
+    void setdata(int i, string n, float s)
+    {
+        id = i;
+        name = n;
+        salary = s;
+    }
+
+    void displaydata()
+    {
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Salary: " << salary << endl;
+    }
+};
+
+int main()
+{
+    employee e1;
+
+    e1.setdata(101, "John Doe", 50000.0);
+    e1.displaydata();
+
+    return 0;
+}
